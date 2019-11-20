8fdbacbcdb79eb126fb22c804461126dc749b594
Steven Vorona <stevenvorona@gmail.com>
2019-11-19 18:46:05 -0800

    added routes

--- application.py
+++ application.py
@@ -16,5 +16,25 @@ def addNewPhoneNumber():
     data = request.data
     return json.dumps(data)
 
+@app.route('/createGroup', methods = ['POST'])
+def createGroup():
+    #host posts phone number to /createGroup, function hashes it to ID, creates file in session directory headed with ID, return hashed ID
+    print(request.data,file=sys.stderr)
+    print(hostPhoneNumber,file=sys.stderr)
+    data = request.data
+    #return style: POST route of http://api/joinGroup?hostphonenumber=hashedHostID
+    return json.dumps(data)
+
+@app.route('/joinGroup', methods = ['POST'])
+def createGroup():
+    hostPhoneNumber = request.args.get('hostphonenumber')
+    #POST friend phone number to http://api/joinGroup?hostphonenumber=hashedHostID
+    print(request.data,file=sys.stderr)
+    #friend phone number is a component of data
+    print(hostPhoneNumber,file=sys.stderr)
+    #write friend phone number into file
+    data = request.data
+    return json.dumps(data)
+
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5000)
